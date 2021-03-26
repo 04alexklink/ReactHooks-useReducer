4d273944d98f75ac72b6e71cd66bb82c3fb87bba
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-26 13:52:10 +0000

    Counter component using useReducer

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CounteruseReducer from './components/CounteruseReducer'
+function App() {
+  return (
+    <div className="App">
+      <CounteruseReducer></CounteruseReducer>
+    </div>
+  );
+}
+
+export default App;

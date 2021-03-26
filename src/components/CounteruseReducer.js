854b4d2772e6c6ec033e8d295cb80ea75f1d0b5e
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-26 14:32:25 +0000

    action object has a value property, which can be used to change state in the reducer

--- src/components/CounteruseReducer.js
+++ src/components/CounteruseReducer.js
@@ -4,9 +4,9 @@ const initialState = { firstCounter: 0}
 const reducer = (state, action)=> {
   switch(action.type) {
     case 'increment':
-      return { firstCounter: state.firstCounter + 1 }
+      return { firstCounter: state.firstCounter + action.value }
     case 'decrement':
-      return { firstCounter: state.firstCounter -1 }
+      return { firstCounter: state.firstCounter - action.value }
     case 'reset':
       return initialState
     default:
@@ -19,9 +19,11 @@ const CounteruseReducer = () => {
     return (
       <div>
         <h1>Count: {count.firstCounter}</h1>
-        <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
-        <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+        <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment by 1</button>
+        <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement by 1</button>
         <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+        <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment by 5</button>
+        <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement by 5</button>
       </div>
     )
 }
